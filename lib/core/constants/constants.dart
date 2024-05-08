bdc2ce6426ccfb43b462e7e722266fad98e145a6
Zettaisha <levvotintsev18@gmail.com>
2024-05-08 21:19:38 +0300

    added more assets, refactor (added icons folder)

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,9 +1,11 @@
 const String ticketsAPIBaseUrl = 'https://run.mocky.io/v3';
 const String musicTicketHeadline = '/00727197-24ae-48a0-bcb3-63eb35d7a9de';
 
-const String bnbPlaneImagePath = 'lib/assets/images/plane_bnb_icon.png';
-const String bnbHotelsImagePath = 'lib/assets/images/hotels_bnb_icon.png';
-const String bnbLocationImagePath = 'lib/assets/images/location_bnb_icon.png';
-const String bnbProfileImagePath = 'lib/assets/images/profile_bnb_icon.png';
+const String bnbPlaneImagePath = 'lib/assets/images/icons/plane_bnb_icon.png';
+const String bnbHotelsImagePath = 'lib/assets/images/icons/hotels_bnb_icon.png';
+const String bnbLocationImagePath =
+    'lib/assets/images/icons/location_bnb_icon.png';
+const String bnbProfileImagePath =
+    'lib/assets/images/icons/profile_bnb_icon.png';
 const String bnbSubscriptionImagePath =
-    'lib/assets/images/subscription_bnb_icon.png';
+    'lib/assets/images/icons/subscription_bnb_icon.png';
