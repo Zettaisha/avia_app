e719b985a6e84e6e2d199789d710f18ff7eb81e3
Zettaisha <levvotintsev18@gmail.com>
2024-05-09 15:31:37 +0300

    home page listview appear fix

--- lib/features/1_home_page/presentation/pages/homepage.dart
+++ lib/features/1_home_page/presentation/pages/homepage.dart
@@ -12,130 +12,133 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   backgroundColor: AppColors.blackColor,
-    //   body: Padding(
-    //     padding: const EdgeInsets.only(top: 35, left: 10, right: 10),
-    //     child: Column(
-    //       children: [
-    //         Center(
-    //           child: Text(
-    //             'Поиск дешевых \nавиабилетов',
-    //             textAlign: TextAlign.center,
-    //             style: TextStyle(
-    //                 color: AppColors.white,
-    //                 fontSize: 27,
-    //                 height: 1.2,
-    //                 fontFamily: fontSfProDisplay,
-    //                 fontWeight: FontWeight.w500),
-    //           ),
-    //         ),
-    //         const SizedBox(
-    //           height: 35,
-    //         ),
-    //         Container(
-    //           width: 350,
-    //           height: 122,
-    //           alignment: Alignment.center,
-    //           decoration: BoxDecoration(
-    //             borderRadius: BorderRadius.circular(16),
-    //             color: AppColors.grey3,
-    //           ),
-    //           child: Container(
-    //             width: 296,
-    //             height: 90,
-    //             decoration: BoxDecoration(
-    //                 color: AppColors.grey4,
-    //                 borderRadius: BorderRadius.circular(16),
-    //                 boxShadow: [
-    //                   BoxShadow(
-    //                       color: Colors.black.withOpacity(0.5),
-    //                       spreadRadius: 5,
-    //                       blurRadius: 7,
-    //                       offset: const Offset(0, 7))
-    //                 ]),
-    //             child: Padding(
-    //               padding: const EdgeInsets.all(15.0),
-    //               child: Row(
-    //                 children: [
-    //                   Padding(
-    //                     padding: const EdgeInsets.only(right: 15),
-    //                     child: ImageIcon(
-    //                       const AssetImage(searchIconImagePath),
-    //                       color: AppColors.white,
-    //                     ),
-    //                   ),
-    //                   Column(
-    //                     mainAxisAlignment: MainAxisAlignment.center,
-    //                     crossAxisAlignment: CrossAxisAlignment.start,
-    //                     children: [
-    //                       Text(
-    //                         'Минск',
-    //                         style: TextStyle(
-    //                             color: AppColors.white,
-    //                             fontSize: 17,
-    //                             fontWeight: FontWeight.bold),
-    //                       ),
-    //                       Text(
-    //                         'Куда - Турция',
-    //                         style: TextStyle(
-    //                             color: AppColors.white,
-    //                             fontSize: 17,
-    //                             fontWeight: FontWeight.bold),
-    //                       )
-    //                     ],
-    //                   )
-    //                 ],
-    //               ),
-    //             ),
-    //           ),
-    //         ),
-    //         const SizedBox(
-    //           height: 30,
-    //         ),
-    //         Align(
-    //           alignment: Alignment.centerLeft,
-    //           child: Text(
-    //             'Музыкально отлететь',
-    //             style: TextStyle(
-    //               color: AppColors.white,
-    //               fontSize: 25,
-    //             ),
-    //           ),
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
-
-    return Scaffold(body: BlocBuilder<MusicTicketsBloc, HomepageState>(
-      builder: (_, state) {
-        if (state is MusicTicketsLoading) {
-          return const Center(
-            child: CupertinoActivityIndicator(),
-          );
-        }
-
-        if (state is MusicTicketsError) {
-          return const Center(
-            child: Icon(Icons.refresh),
-          );
-        }
-        if (state is MusicTicketsDone) {
-          return ListView.builder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            itemCount: state.musicTickets!.length,
-            itemBuilder: (context, index) {
-              var musicTicket = state.musicTickets![index];
-              return MusicTicketCard(
-                musicTicket: musicTicket,
+    return Scaffold(
+        backgroundColor: AppColors.blackColor,
+        body: BlocBuilder<MusicTicketsBloc, HomepageState>(
+          builder: (_, state) {
+            if (state is MusicTicketsLoading) {
+              return const Center(
+                child: CupertinoActivityIndicator(),
               );
-            },
-          );
-        }
-        return const SizedBox();
-      },
-    ));
+            }
+
+            if (state is MusicTicketsError) {
+              return const Center(
+                child: Icon(Icons.refresh),
+              );
+            }
+            if (state is MusicTicketsDone) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 35, left: 10, right: 10),
+                child: Column(
+                  children: [
+                    Center(
+                      child: Text(
+                        'Поиск дешевых \nавиабилетов',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: AppColors.white,
+                            fontSize: 27,
+                            height: 1.2,
+                            fontFamily: fontSfProDisplay,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 35,
+                    ),
+                    Container(
+                      width: 350,
+                      height: 122,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: AppColors.grey3,
+                      ),
+                      child: Container(
+                        width: 296,
+                        height: 90,
+                        decoration: BoxDecoration(
+                            color: AppColors.grey4,
+                            borderRadius: BorderRadius.circular(16),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: const Offset(0, 7))
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 15),
+                                child: ImageIcon(
+                                  const AssetImage(searchIconImagePath),
+                                  color: AppColors.white,
+                                ),
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Минск',
+                                    style: TextStyle(
+                                        color: AppColors.white,
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    'Куда - Турция',
+                                    style: TextStyle(
+                                        color: AppColors.white,
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Музыкально отлететь',
+                        style: TextStyle(
+                          color: AppColors.white,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                      height: 230, // Установите нужную высоту здесь
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: state.musicTickets!.length,
+                        itemBuilder: (context, index) {
+                          var musicTicket = state.musicTickets![index];
+                          return MusicTicketCard(
+                            musicTicket: musicTicket,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+            return const SizedBox();
+          },
+        ));
   }
 }
--- lib/features/1_home_page/presentation/widgets/music_ticket_card.dart
+++ lib/features/1_home_page/presentation/widgets/music_ticket_card.dart
@@ -1,3 +1,4 @@
+import 'package:avia_app/config/themes/colors.dart';
 import 'package:avia_app/core/constants/constants.dart';
 import 'package:avia_app/features/1_home_page/domain/enitites/music_ticket.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,28 +10,60 @@ class MusicTicketCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        ClipRRect(
-          borderRadius:
-              BorderRadius.circular(16), // Задаем радиус закругления углов
-          child: Image.asset(
-            'lib/assets/images/music_ticket_${musicTicket!.id!}.jpg',
-            height: 132,
-            width: 133,
-            fit: BoxFit.fill,
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.asset(
+                  'lib/assets/images/music_ticket_${musicTicket!.id!}.jpg',
+                  height: 132,
+                  width: 133,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                musicTicket!.title!,
+                style: TextStyle(
+                    color: AppColors.white,
+                    fontFamily: fontSfProDisplay,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                musicTicket!.town!,
+                style: TextStyle(
+                    color: AppColors.white,
+                    fontFamily: fontSfProDisplay,
+                    fontSize: 14),
+              ),
+              Row(
+                children: [
+                  ImageIcon(
+                    const AssetImage(bnbPlaneImagePath),
+                    color: AppColors.white,
+                  ),
+                  Text(
+                    'от ${musicTicket!.price} ₽',
+                    style: TextStyle(
+                        color: AppColors.white,
+                        fontFamily: fontSfProDisplay,
+                        fontSize: 16),
+                  )
+                ],
+              )
+            ],
           ),
-        ),
-        Text(
-          musicTicket!.title!,
-        ),
-        Text(musicTicket!.town!),
-        Row(
-          children: [
-            const ImageIcon(AssetImage(bnbPlaneImagePath)),
-            Text('от ${musicTicket!.price} ₽')
-          ],
         )
       ],
     );
